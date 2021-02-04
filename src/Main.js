71980b0bfaa1cc2bb5529b4b027b31b96d92af93
Benjamin Barnard <barnardwben@yahoo.com>
2021-02-04 19:59:08 +0100

    Created Main component

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Main.css";
+const Main = () => {
+  return (
+    <>
+      <main className="home-main">
+        <section className="hmain-section-one">
+          <div className="left-grid-one"></div>
+          <div className="right-grid-one"></div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Main;
