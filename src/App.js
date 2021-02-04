b2aee9ef834d6417d7e97d440619ef19909257fb
Benjamin Barnard <barnardwben@yahoo.com>
2021-02-04 19:57:51 +0100

    Added Main component to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Nav from "./Nav";
 import Header from "./Header";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+      <Main />
     </div>
   );
 }
