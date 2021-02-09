f3360277466b0e9fe369801faa6134202acb3af4
Benjamin Barnard <barnardwben@yahoo.com>
2021-02-09 17:58:51 +0100

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./Nav";
-import Header from "./Header";
-import Main from "./Main";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 import Footer from "./Footer";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Nav />
-      <Header />
-      <Main />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
