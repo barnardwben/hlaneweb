816c94bc2d4c1db62303bd6e7d675cb15ba0d366
Benjamin Barnard <barnardwben@yahoo.com>
2021-03-03 21:07:11 +0100

    Updated HomeHeader Component

--- src/HomeHeader.js
+++ src/HomeHeader.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./HomeHeader.css";
 
 const HomeHeader = () => {
+  const homeHeaderRef = useRef(null);
+
+  const headerScroll = () => {
+    if (
+      window.location.href === "http://localhost:3000" ||
+      window.location.href === "http://localhost:3000/"
+    ) {
+      if (window.scrollY > 0) {
+        document.querySelector(
+          ".homeheader-bg"
+        ).style.backgroundPositionY = `-${200 + window.scrollY / 5}px`;
+      }
+      if (window.scrollY > 1400) {
+        homeHeaderRef.current.classList.add("headerscroll");
+      } else if (window.scrollY < 1400) {
+        homeHeaderRef.current.classList.remove("headerscroll");
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", headerScroll);
+
+    return () => {
+      window.removeEventListener("scroll", headerScroll);
+    };
+  });
+
   return (
     <>
-      <header>
+      <header className="homeheader-bg" ref={homeHeaderRef}>
         <div className="header-cover-layer">
           <div className="header-text-container">
             <h1 className="header-one">Life Changing Moves Made Easy</h1>
             <h2 className="header-two">Hannah Lane</h2>
             <p className="header-para">Twin Cities Realtor®</p>
           </div>
+          <img
+            src="hannahimages\cuthannah.png"
+            alt="Hannah Lane Realtor"
+            className="cutimagehannah"
+          />
         </div>
       </header>
     </>
