74d249f1fe28c67f628465230f9a588be3f1197b
Benjamin Barnard <barnardwben@yahoo.com>
2021-03-03 21:06:34 +0100

    Updated About Component

--- src/About.js
+++ src/About.js
@@ -1,7 +1,14 @@
 import React from "react";
+import AboutHeader from "./AboutHeader";
+import AboutMain from "./AboutMain";
 import "./About.css";
 const About = () => {
-  return <div></div>;
+  return (
+    <>
+      {/* <AboutHeader /> */}
+      <AboutMain />
+    </>
+  );
 };
 
 export default About;
