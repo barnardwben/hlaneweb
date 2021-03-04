b6cfdeafabe547911fd748046053aa608bd68345
Benjamin Barnard <barnardwben@yahoo.com>
2021-03-04 21:09:45 +0100

    Updated App

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./Nav";
 import Home from "./Home";
+import Minneapolis from "./Minneapolis";
+import StPaul from "./StPaul";
 import About from "./About";
-import Contact from "./Contact";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <Router>
-      <Nav />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-      </Switch>
-      <Footer />
+      <ScrollToTop>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/minneapolis">
+            <Minneapolis />
+          </Route>
+          <Route exact path="/st.paul">
+            <StPaul />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+        <Footer />
+      </ScrollToTop>
     </Router>
   );
 }
