a4b48ed6acd59ae7f20b4bf77a3d2369a17986ef
Benjamin Barnard <barnardwben@yahoo.com>
2021-03-06 21:18:52 +0100

    Updated AboutHeader Component

---
+++ src/AboutHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./AboutHeader.css";
+
+const AboutHeader = () => {
+  return (
+    <>
+      <header className="about-header">
+        <img src="hannahimages\panocity.png" alt="" className="abouthdr-img" />
+        <div className="bg-overlay"></div>
+      </header>
+    </>
+  );
+};
+
+export default AboutHeader;
