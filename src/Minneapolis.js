aa5601081e14fc39523243b4bc142f4887a44693
Benjamin Barnard <barnardwben@yahoo.com>
2021-03-07 21:22:20 +0100

    Added Minneapolis Component

---
+++ src/Minneapolis.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MinnHeader from "./MinnHeader";
+import MinnMain from "./MinnMain";
+
+const Minneapolis = () => {
+  return (
+    <>
+      <MinnHeader />
+      <MinnMain />
+    </>
+  );
+};
+
+export default Minneapolis;
