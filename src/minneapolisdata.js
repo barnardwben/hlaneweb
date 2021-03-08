ec0a0d4d3ffd2b51e249353152449f96448f5f8a
Benjamin Barnard <barnardwben@yahoo.com>
2021-03-08 21:24:44 +0100

    Added minneapolis data

---
+++ src/minneapolisdata.js
@@ -0,0 +1,90 @@
+const minneapolisdata = [
+  {
+    key: "first",
+    id: 1,
+    locationtitle: "Mill District Directory",
+    src:
+      "https://www.google.com/maps/d/embed?mid=1_VrZCNe3mKwgxIZ0PNceciEWMMv1zlhX",
+    desc:
+      "Minneapolis's Mill District - From the early 1900s to the late 1930s this district was hustling and bustling with numerous flour mills, which earned it the recognition of being the flour milling capital of the world. Today it's an area revered for its diversified culture and history, with many of the aforementioned flour mills having been converted into distinguished lofts and condominiums.",
+  },
+  {
+    key: "second",
+    id: 2,
+    locationtitle: "North Loop Directory",
+    src:
+      "https://www.google.com/maps/d/embed?mid=1tqRao3i2-XkvRq7vZiKG_PWLZ6FAkf7X",
+  },
+  {
+    key: "third",
+    id: 3,
+    locationtitle: "Old Town (Nicollet Island/East Bank)",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1dmvE0eSkHL51UPYa6YlJ-bOwnRZ1cnal",
+  },
+  {
+    key: "fourth",
+    id: 4,
+    locationtitle: "Central Business Districts",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1RZO3WuEHtFvAcGdhl0hSVlvANAIHSSYv",
+  },
+  {
+    key: "fifth",
+    id: 5,
+    locationtitle: "Elliot Park",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=16bfdgk3gWbcNX38Lox4mwy73izIfgpH7",
+  },
+  {
+    key: "six",
+    id: 6,
+    locationtitle: "Loring Park",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1a18aQDCOfu_A1n7Mx-3PGmv4sI1HaWBy",
+  },
+  {
+    key: "seven",
+    id: 7,
+    locationtitle: "Kenwood/Lowry Hill",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=16ll2UFozsqx3CdGtZCtqdHIupp4F0ndl",
+  },
+  {
+    key: "eight",
+    id: 8,
+    locationtitle: "Lakes / Uptown / Lyn-Lake",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1NbnYdqiq7T-9RS6YxuOC4J7TgxIozKMv",
+  },
+  {
+    key: "nine",
+    id: 9,
+    locationtitle: "Northeast Minneapolis",
+    src:
+      "https://www.google.com/maps/d/embed?mid=1L0avLsiRmzVYTikScbS9xigjY7uRqQOg&hl=en",
+  },
+  {
+    key: "ten",
+    id: 10,
+    locationtitle: "University of MN",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1yTw45nyliI1dt8Qibmpipju9g6zDfnO0",
+  },
+  {
+    key: "eleven",
+    id: 11,
+    locationtitle: "Marcy Holmes",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1kkx4McgEKCnVoa6IU7RFI-IO0F9mK4Nq",
+  },
+  {
+    key: "twelve",
+    id: 12,
+    locationtitle: "Prospect Park",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1b7CvGtDgzj1nNUKsETUL0LWiAcFf_umb",
+  },
+];
+
+export default minneapolisdata;
