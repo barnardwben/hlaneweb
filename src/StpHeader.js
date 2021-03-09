3e7a2843ba08f6250063dc1e33253b450ba8f906
Benjamin Barnard <barnardwben@yahoo.com>
2021-03-09 21:27:28 +0100

    Added StpHeader Component and Styling

---
+++ src/StpHeader.js
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect } from "react";
+import "./StpHeader.css";
+
+const StpHeader = () => {
+  const minnBgRef = useRef(null);
+
+  const minnbgScroll = () => {
+    if (
+      window.location.href === "http://localhost:3000/st.paul" ||
+      window.location.href === "http://localhost:3000/st.paul/"
+    ) {
+      if (window.scrollY > 500) {
+        minnBgRef.current.classList.add("minnbgscroll");
+      } else if (window.scrollY < 500) {
+        minnBgRef.current.classList.remove("minnbgscroll");
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", minnbgScroll);
+
+    return () => {
+      window.removeEventListener("scroll", minnbgScroll);
+    };
+  });
+
+  return (
+    <>
+      <header className="minneapolis-header" ref={minnBgRef}>
+        <img src="hannahimages\stpbg.jpeg" alt="" className="minnbg" />
+        <div className="minnheader-bg-cover"></div>
+      </header>
+    </>
+  );
+};
+
+export default StpHeader;
