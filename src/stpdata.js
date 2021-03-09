bc441adf7ff378702d9c9aeb29ac3f6a99981279
Benjamin Barnard <barnardwben@yahoo.com>
2021-03-09 21:27:59 +0100

    Added stpdata

---
+++ src/stpdata.js
@@ -0,0 +1,92 @@
+const stpdata = [
+  {
+    key: "thirteen",
+    id: 13,
+    locationtitle: "Downtown - Lowertown",
+    src:
+      "https://www.google.com/maps/d/embed?mid=152eLY3z26PQGh6NfPl5_CYqBlenbPj0E",
+    desc: "",
+  },
+  {
+    key: "fourteen",
+    id: 14,
+    locationtitle: "Downtown - Wacouta Commons",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1D3Y1Pf-6PxnsRm7COmX6GAgGplo86iOz",
+    desc: "",
+  },
+  {
+    key: "fifteen",
+    id: 15,
+    locationtitle: "Downtown - Fitzgerald Park",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1AKBvCfwpCZJmw6Tu0UUOdUoR9ikMJGcJ",
+    desc: "",
+  },
+  {
+    key: "sixteen",
+    id: 16,
+    locationtitle: "Downtown - Irvine Park",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1cd5tKzwaKbrWNxQ98Z0q3j7zUj7kySJN",
+    desc: "",
+  },
+  {
+    key: "seventeen",
+    id: 17,
+    locationtitle: "Downtown - Rice Park",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1lV8DJO8srH0RyMl4vXVRYqymquyNA655",
+    desc: "",
+  },
+  {
+    key: "eighteen",
+    id: 18,
+    locationtitle: "Downtown - Upper Landings",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1-X6uTKQGbLjYE77okg0eMcIoVQehwFk2",
+    desc: "",
+  },
+  {
+    key: "nineteen",
+    id: 19,
+    locationtitle: "Summit Hill - Ramsey Hill",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1HLPw3kfQ4udyvBMV4EWSmxAtpPBmE0Gg",
+    desc: "",
+  },
+  {
+    key: "twenty",
+    id: 20,
+    locationtitle: "Summit Hill - Crocus Hill",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1H_JwSY2Gih1DhEdT7vCQuKkgB7QQSUqs",
+    desc: "",
+  },
+  {
+    key: "twentyone",
+    id: 21,
+    locationtitle: "Summit Hill - Cathedral Hill",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1AcLfA0GXPDPtWdd6bD1LAZCIlzPHyfgp",
+    desc: "",
+  },
+  {
+    key: "twentytwo",
+    id: 22,
+    locationtitle: "Highland Park",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1hxVE1zDcxIqA98XySiIfYkiU9ajaj0fq",
+    desc: "",
+  },
+  {
+    key: "twentythree",
+    id: 23,
+    locationtitle: "St. Anthony Park",
+    src:
+      "https://www.google.com/maps/d/u/0/embed?mid=1N1M6rV7ZE5S_Ek5tGFU7JiBllegYncKj",
+    desc: "",
+  },
+];
+
+export default stpdata;
